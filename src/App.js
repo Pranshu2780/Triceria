5a1d9450517ce93ad3670cd54eab9dbe363860f6
Milind Pandey (Captain Levi) <47604864+Pranshu2780@users.noreply.github.com>
2020-07-30 17:02:05 +0530

    second

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ function App() {
   return (
     <div className="App">
       
+      {/* Navigation Bar Start */}
+      
+      <header>
+           <div className="name"> Guessing Game </div>
+           <nav className="nac">
+              <ul className="navbar" >
+                   <li> <a href="#"> Home </a> </li>
+                   <li> <a href="#"> Rules </a> </li>  
+              </ul>
+           </nav> 
+       </header>    
+      
+       {/* Navigation Bar End */}
+     
+       <input className="input" type="text" name="entering" />
+       <button className="btn" > Enter </button> 
+    
     </div>
   );
 }
